64bdd54d7a0386a80e74e78b922d5d981bea4fcf
romanolivi <romano_livi@outlook.com>
2021-03-26 16:13:16 -0400

    commit tree 1e38ef0d5e5e51f96cfe9def1b9d0250605d6a8d

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-const testVar = {}
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+]
 
-function testFunc() {
-  return "hi"
+function superbowlWin(array) {
+  let winResult = array.find(w => w.result === "W")
+  if (winResult) {
+    return winResult.year
+  } else {
+    return undefined
+  };
 }
